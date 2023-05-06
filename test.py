2a5817b3a5aba6268a03536280f635723edf770e
Xin Liu <sam@secondstate.io>
2023-05-06 13:49:24 +0800

    add test script
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- test.py
+++ test.py
@@ -2,25 +2,30 @@ import msgpack
 import modelz
 
 APIKey = "mzi-510edb7dff018ec70bc8e82671552dcc"
-DeploymentKey = "mist-g558664cd4aew1tl"
+DeploymentKey = "mist-no7v6zho9zgwvzko"
+timeout = 60 * 15
 
 # create a modelz client
-cli = modelz.ModelzClient(key=APIKey, deployment=DeploymentKey, timeout=60 * 15)
-
-params = msgpack.packb(
-    {
-        "image_url": "https://raw.githubusercontent.com/apepkuss/mist/main/test/sample.png",
-        "output_size": 512,
-        "epsilon": 16,
-        "steps": 100,
-        "block_num": 1,
-        "mode": 2,
-        "rate": 1,
-    }
+cli = modelz.ModelzClient(
+    key=APIKey,
+    deployment=DeploymentKey,
+    host="https://mist-no7v6zho9zgwvzko.modelz.io/",
+    timeout=timeout,
 )
 
+params = {
+    "image_url": "https://raw.githubusercontent.com/apepkuss/mist/main/test/sample.png",
+    "output_size": 512,
+    "epsilon": 16,
+    "steps": 100,
+    "block_num": 1,
+    "mode": 2,
+    "rate": 1,
+}
+
+
 print("*** Performing inference ***")
-response = cli.inference(params=params, serde="msgpack")
+response = cli.inference(params=params, serde="json")
 
 if response.status_code == 200:
     print(msgpack.unpackb(response.content))
